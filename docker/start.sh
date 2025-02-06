2fe3c521fc3fdb4d6f0710aefca9f6b706e168af
Ali Aliev <ali@avatarify.ai>
2025-02-06 14:14:38 +0000

    self-signed certificate

--- docker/start.sh
+++ docker/start.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# Uncomment to generate a self-signed certificate
+# mkcert -install
+# mkcert -key-file key.pem -cert-file cert.pem localhost 127.0.0.1 ::1 *.localhost 0.0.0.0
+
 cd /home/user/voi-js-client
 npm install
 npm run serve
